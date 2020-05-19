928fd5b3b6effe5eecd07316d4dfdc3f367a3767
Adri Rojo <adriamilmarcos@gmail.com>
2020-05-19 17:02:04 +0200

    Added Favourite and Custom Pokemon to User and Models

---
+++ PokedexAPI/Models/FavouritePokemonModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokedexAPI.Models
+{
+    public class FavouritePokemonModel
+    {
+        public int Id { get; set; }
+        public int FavouritePokemonId { get; set; }
+    }
+}
